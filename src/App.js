371a021f8bace29bae106ad0e2622ccc868f1feb
Manuel G <manuel.geissberger@gmail.com>
2023-12-15 10:09:40 +0100

    env vriables and api access

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return <div>Hello, ToDo!</div>;
+  const [response, setResponse] = useState();
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/todos`, {
+      method: "GET",
+    }).then(async (response) => {
+      if (response.ok) {
+        setResponse(await response.json());
+      } else {
+        setResponse("error while loading");
+      }
+    });
+  }, []);
+
+  return <div>{response}</div>;
 };
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -13,26 +14,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
+    }),
+    new Dotenv({
+      path: `./.env.${process.env.NODE_ENV}`, // Path to .env file (this is the default)
+      safe: true, // load .env.example (defaults to "false" which does not use dotenv-safe)
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     hot: true,
     open: true,
